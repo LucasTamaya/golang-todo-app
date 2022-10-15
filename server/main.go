b00953197adb4838964e1b9fd870ba91945182a0
LucasTamaya <lucastamaya.974@gmail.com>
2022-10-15 09:20:51 +0400

    Basic UI for todo app
    - created the form
    - handle input state

--- server/main.go
+++ server/main.go
@@ -6,17 +6,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// func getBase(ctx *fiber.Ctx) error {
-// 	return ctx.Status(fiber.StatusOK).SendString("Welcome to my rest api")
-// }
-
 // struct are similar to object in JS
 type Todo struct {
 	// quotes will be used to send json response
-	ID    int    `json:"id"`
+	ID    string `json:"id"`
 	Title string `json:"title"`
-	Done  bool   `json:"done"`
 	Body  string `json:"body"`
+	Done  bool   `json:"done"`
 }
 
 var Todos = []Todo{}
@@ -24,14 +20,6 @@ var Todos = []Todo{}
 func main() {
 	// init the app
 	app := fiber.New()
-
-	// var data int
-
-	// if data != nil {
-	// 	fmt.Println("The data is not nil")
-	// } else {
-	// 	fmt.Println("The data is nil")
-	// }
 
 	app.Get("/todos", GetAllTodos)
 	app.Get("/todo/:id", GetTodo)
